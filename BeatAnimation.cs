158292924423f973b5a8830cdbdd46c31fc9e25b
KaikiKazuyoshi <colaiiiyahhouy@gmail.com>
2019-05-06 21:49:00 +0900

    first commit

---
+++ BeatAnimation.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace  MusicBeat
+{
+	public class BeatAnimation : MonoBehaviour {
+
+		public AudioSource audioSorce;
+		public float Bpm = 140;
+
+		public float minSize;
+		public float maxSize;
+
+		float playtime;
+		float beatUnitPerSec;
+		float beats;
+		float bar;
+		float animationScale;
+		
+		private void Start() {
+			beatUnitPerSec = 60.0f/Bpm;
+		}
+		void Update () {
+			playtime = audioSorce.time;
+			beats = playtime/beatUnitPerSec;
+			bar = beats*0.25f;
+			float animationScale = GetAnimationTime(beats,minSize,maxSize);
+			transform.localScale = Vector3.one*animationScale;
+		}
+
+		float　GetAnimationTime(float beats,float curvMin,float curvMax){
+			float musicSyncedValue = Mathf.Cos(2*Mathf.PI*beats);
+			musicSyncedValue += 1;
+			musicSyncedValue *= 0.5f;
+			return Mathf.Lerp(curvMin,curvMax,musicSyncedValue);
+		}
+	}
+}
---
+++ BeatChecker.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MusicBeat
+{
+	public class BeatChecker : MonoBehaviour {
+
+		public AudioSource audioSorce;
+		public Material material;
+		public float Bpm = 140;
+		float playtime;
+		float beatUnitPerSec;
+		float prevBeats;
+		float prevBar;
+		float animationScale;
+		float count;
+		
+		private void Start() {
+			beatUnitPerSec = 60.0f/Bpm;
+		}
+		void Update () {
+			playtime = audioSorce.time;
+			float beats = Mathf.CeilToInt(playtime/beatUnitPerSec);
+			float bar = Mathf.CeilToInt(beats*0.25f);
+			
+			bool changeBeat = prevBeats != beats;
+			bool changeBar = prevBar != bar;
+
+			if (changeBeat){
+				if(material.HasProperty("_Color")){
+					if (count == 0){
+						material.SetColor("_Color", Color.green);
+						count++;
+					} else if (count == 1){
+						material.SetColor("_Color", Color.blue);
+						count++;
+					} else if (count == 2){
+						material.SetColor("_Color", Color.yellow);
+						count = 0;
+					}
+				}
+			}
+			if (changeBar){
+				
+			}
+			prevBeats = beats;
+			prevBar = bar;
+		}
+	}
+}
